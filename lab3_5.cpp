607dd6a87f595e79c181466e084f0b7187488158
lab302-4 <lab302-4@labeng.local>
2019-01-11 17:12:51 +0700

    lab3_5 v1

---
+++ lab3_5.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string text1;
+    string text2;
+    int count;
+    count=0;
+    int N;
+
+cout<<"Enter the firt text:";
+cin>>text1;
+cout<<"\nEnter the second text:";
+cin>>text2;
+cout<<"\nEnter N:";
+cin>>N;
+     while(count<N){
+         if(count%2!=0){
+             cout<<text1<<" ";
+         }else{
+             cout<<text2<<" ";
+         }
+          count = count + 1;
+     }
+     return 0;
+}
